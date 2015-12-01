a4d4eae5e6f8237d792f3b39a4eae6582c2b7ba8
ericgiannini <eric.giannini@yahoo.com>
2015-12-01 10:24:15 -0800

    Dispatch UIAlert on Main Thread for Local Notification

--- PushNotificationApp/AppDelegate.m
+++ PushNotificationApp/AppDelegate.m
@@ -20,6 +20,22 @@
     return YES;
 }
 
+- (void) application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    
+    UIAlertController *alertReceptionOfLocalNotification = [UIAlertController alertControllerWithTitle:@"Local Notification Received at Runtime" message:notification.alertBody preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Okay" style:UIAlertActionStyleDefault handler:nil];
+    
+    [alertReceptionOfLocalNotification addAction:action];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        [application.keyWindow.rootViewController presentViewController:alertReceptionOfLocalNotification animated:YES completion:nil];
+    });
+    
+    
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
