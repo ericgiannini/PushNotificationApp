f0794fc02cb6b04a545c05e09ed3cc793e0fe0d8
ericgiannini <eric.giannini@yahoo.com>
2015-12-01 09:52:30 -0800

    Adding Application Delegate

---
+++ PushNotificationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushNotificationApp
+//
+//  Created by Eric Giannini on 12/1/15.
+//  Copyright © 2015 Unicorn Mobile, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
